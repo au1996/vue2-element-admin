487d15499022bae7a4ee40cdcb7a377914c221e4
au1996 <15119376155@163.com>
2021-06-27 17:58:38 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# 发生任何错误时终止
+set -e
+
+# 部署
+npm run build
+git add .
+git commit -m 'deploy'
+git push origin master
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:au1996/vue2-element-admin.git master:gh-pages
+
+cd -
