030fedb578a891e7ae015cc742b1ab412425c55a
gujinzhu <15119376155@163.com>
2022-05-26 17:28:52 +0800

    feat: :zap: add workflows

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,8 @@
 # 发生任何错误时终止
 set -e
 
-# 部署
-npm run build
 git add .
-git commit -m 'feat: :art: update draggable directive'
+git commit -m 'feat: :zap: add workflows'
 git push origin master
-git subtree push --prefix dist origin gh-pages
-
-# 如果你要部署在 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:au1996/vue2-element-admin.git master:gh-pages
 
 cd -
